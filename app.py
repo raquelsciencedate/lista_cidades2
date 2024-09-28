63d8790eb646d6db50ad04b304bc55d92defe354
raquelsciencedate <lopesadvogados1965@gmail.com>
2024-09-27 21:19:13 -0300

    versao 1.0

---
+++ app.py
@@ -0,0 +1,29 @@
+#lista cidades
+
+cidades_brasileiras = [
+    "São Paulo",
+    "Rio de Janeiro",
+    "Belo Horizonte",
+    "Brasília",
+    "Salvador",
+    "Fortaleza",
+    "Curitiba",
+    "Manaus",
+    "Recife",
+    "Porto Alegre",
+    "Belém",
+    "Goiânia",
+    "Campinas",
+    "São Luís",
+    "Maceió"
+]
+print(cidades_brasileiras)
+
+# lista em ordem alfabetica
+cidades_brasileiras.sort()
+
+print("lista em ordem alfabetica:\n")
+for nome in cidades_brasileiras:
+    print(cidades_brasileiras)
+    
+
